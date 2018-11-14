dad438ca8c989ca7ae26bace84ec2d77e78d6179
Doobs Parn <loganrweber@gmail.com>
2018-11-14 15:58:58 -0800

    Add .vimrc_background

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 
 dir="$HOME/Dotfiles"
 backup_dir="$HOME/Dotfiles.bak"
-files="gitconfig ideavimrc spacemacs vimrc zshrc"
+files="gitconfig ideavimrc spacemacs vimrc vimrc_background zshrc"
 
 echo "creating $backup_dir ..."
 mkdir "$backup_dir"
